c96a689f1213c140bb42499a1421ed4b63465652
Ruth Tshoms <tshomsruth@gmail.com>
2022-06-23 16:39:03 +0200

    ajout

--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@ async function recupererCanape() {
   else {
     let donnees = await requete.json();
     console.log(donnees); // vérifier que les données demandées nous soient bien retournées (voir dans la console du navigateur) //
-    for(let i = 0; i > 0; i++) {
+    for(let i = 0; i > 0; i++) { // parcourir les objects pour avoir toutes les infos, les détails afin de les afficher //
       console.log(donnees[0]);
     }
-    // parcourir les objects pour avoir toutes les infos, les détails afin de les afficher //
+    
     document.querySelector('span').textContent = donnees.Canape.last; // voir le cours DOM le js va parcourir le dom pour recup le span // 
   }
 }
