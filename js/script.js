66843404fb4b4653f8905feca649f097771b583c
Ruth Tshoms <tshomsruth@gmail.com>
2022-06-29 12:58:09 +0200

    29 juin 2022 - destructuring assignment des données (img, nom, description) et affichage dans la console OK

--- js/script.js
+++ js/script.js
@@ -11,15 +11,21 @@ async function recupererCanape() {
   else {
     let donnees = await requete.json();
     console.log(donnees); // vérifier que les données demandées nous soient bien retournées (voir dans la console du navigateur) //
-    for(let i = 0; i > 0; i++) { // parcourir les objects pour avoir toutes les infos, les détails afin de les afficher //
-      console.log(donnees[0]);
-    }
-    
-    document.querySelector('img').innerHTML = ; // js va parcourir le dom pour recup l'img  // 
+    //for(let i = 0; i > 0; i++) { // parcourir les objects pour avoir toutes les infos, les détails afin de les afficher //
+     // console.log(donnees[0]);
+    // }
+    let informations = ['http://localhost:3000/images/kanap01.jpeg', 'Kanap Sinopé', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'];
+    let [imageUrl, name, description] = informations; 
+    console.log(imageUrl);
+    console.log(name);
+    console.log(description);
+
+
+    // document.querySelector('img').innerHTML = // js va parcourir le dom pour recup l'img UTILISER AFFECTION POUR MANIP // 
     // document.querySelector('.h3');
     // document.querySelector('.p');
 
-    console.log('img');
+    // console.log('img');
   }
 }
 
