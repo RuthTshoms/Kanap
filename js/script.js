0a753a46d0e62994207ad319b2ebe5f434e0f21b
Ruth Tshoms <tshomsruth@gmail.com>
2022-07-01 15:46:38 +0200

    1 juillet 2022 - modification du dom et affichage des produits page d'accueil ok

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const url = 'http://localhost:3000/api/products';
 
-async function recupererCanape() {
+async function recupererCanape() { // fonction asynchrone *****
   const requete = await fetch(url, { //  attendre que fetch se termine pour récupérer notre requete //
     method: 'GET'
   });
@@ -12,25 +12,22 @@ async function recupererCanape() {
     let donnees = await requete.json(); // attendre que fetch termine de convertir les données en json pour les récupérer //
     // console.log(donnees); // vérifier que les données demandées nous soient bien retournées (console du navigateur) //
 
-    //for(let i = 0; i > 0; i++) { // parcourir les objects pour avoir toutes les infos, les détails afin de les afficher //
-    // console.log(donnees[0]);
-    // }
+    let kanapHTML = ""; // variable vide qui contiendra chaque ******
+    donnees.forEach(function (donnees) { // stockage de l'élément en cours dans la variable "donnees"
+      kanapHTML += `<a href="./product.html?id=${donnees._id}"> 
+      <article>
+        <img src="${donnees.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+        <h3 class="productName">${donnees.name}Kanap name1</h3>
+        <p class="productDescription">${donnees.description}Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada
+          risus sapien gravida nulla nisl arcu.</p>
+      </article>
+    </a>` // on ajoute à chaque itération les valeurs des éléments ajoutés dynamiquement
 
-    //for (const x of donnees) {
-    //  console.log(x);
-    // }
-
-    let { imageUrl, name, description } = donnees;
-console.log(donnees)
-    console.log(imageUrl);
-    console.log(name);
-    console.log(description);
-
-    document.querySelector('img').innerHTML = imageUrl;// js va parcourir le dom pour recup la balise souhaitée, ici l'img, et la modifier  // 
-    document.querySelector('.h3').innerHTML = name;
-    document.querySelector('.p').innerHTML = description;
+    })
+    items.innerHTML = kanapHTML; // reprise du DOM et insertion de kanaHTML à chaque fois (c'est ici que kanapHTml sera changé)
 
   }
+
 }
 
 recupererCanape();
