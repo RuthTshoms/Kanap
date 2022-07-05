47bb32f853520e93ea0252ceab57b5dab855899d
Ruth Tshoms <tshomsruth@gmail.com>
2022-07-05 14:08:05 +0200

    5 juillet 2022 - récupération id, requête fetch id

---
+++ js/product.js
@@ -0,0 +1,25 @@
+// Extraire l'id //
+let params = new URL(document.location).searchParams; // - parcourir et afficher les param de la page
+// console.log(document.location); // OK
+let id = params.get("id"); // récupérer la valeur de l'id attribuée à la variable id //
+// console.log(id); // OK
+
+// Affichage du produit (objet) sélectionné par l'id en faisant une requête fetch //
+async function recupererId() {
+  const requete = await fetch(`http://localhost:3000/api/products/${id}`,{ // récupérer dynamiquement l'id de l'élément pour lequel on effectue une requete
+    method: 'GET'
+  });
+  // console.log(requete); // afficher l'id dans la console - "status 200" requete OK //
+  
+
+  if (!requete.ok) {
+    alert('Un problème est survenu.');
+  }
+  else { // selection du DOM où
+    let donnees = await requete.json();
+    // console.log(donnees); // verifier infos complètes du produit OK //
+    
+  }
+}
+
+recupererId();
