8aee39170d5359aa955c52da64eb544ae7ccc904
Ruth Tshoms <tshomsruth@gmail.com>
2022-07-06 16:14:50 +0200

    affichage infos de chaque canapé OK

--- js/product.js
+++ js/product.js
@@ -19,41 +19,16 @@ async function recupererId() {
     let donneesId = await requete.json();
     // console.log(donnees); //vérifie que les infos complètes du produit nous soient bien retournées OK //
     
-    let image = document.querySelector('.item__img');
-    let h1 = document.querySelector('#title');
-    let prix = document.querySelector('#price');
-    let description = document.querySelector('#description');
+    document.querySelector('.item__img').innerHTML += `<img src="${donneesId.imageUrl}" alt="Photographie d'un canapé"></img>`;
+    document.querySelector('#title').innerHTML = `<h1 id="title">${donneesId.name}</h1>`;
+    document.querySelector('#price').innerHTML = `<p>Prix : <span id="price">${donneesId.price}</span>€</p>`;
+    document.querySelector('#description').innerHTML = `<p id="description">${donneesId.description}</p>`;
 
-    console.log(image); // sélection DOM OK
-    console.log(h1); // OK
-    console.log(prix); // OK
-    console.log(description); // OK
+    
 
-    let affichageKanap = "" // bien découper et individualiser les parties du DOM à modifier //
-    // ajouter une boucle qui parcour le produit avec un += mais bon  //
-
-    affichageKanap = `<div class="item__img"> 
-    <img src="${donneesId.imageUrl}" alt="Photographie d'un canapé">
-  </div>
-  <div class="item__content">
-
-    <div class="item__content__titlePrice">
-      <h1 id="title">${donneesId.name}</h1>
-      <p>Prix : <span id="price">${donneesId.price}</span>€</p>
-    </div>
-
-    <div class="item__content__description">
-      <p class="item__content__description__title">Description :</p>
-      <p id="description">${donneesId.description}</p>
-    </div>`
+    
 
   }
-  //item__img.innerHTML = affichageKanap;
-  //title.innerHTML = affichageKanap;
-  //price.innerHTML = affichageKanap;
-  //description.innerHTML = affichageKanap;
-
- item.innerHTML = affichageKanap;
-
+// hors de la boucle //
 }
 recupererId();
