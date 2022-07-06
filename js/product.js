b7b32432adf44c54121b9a20264b0065663b6a3c
Ruth Tshoms <tshomsruth@gmail.com>
2022-07-06 11:50:03 +0200

    6 juillet 2022 - accès DOM éléments à insérer OK

--- js/product.js
+++ js/product.js
@@ -9,16 +9,28 @@ async function recupererId() {
   const requete = await fetch(`http://localhost:3000/api/products/${id}`,{ // récupérer dynamiquement l'id de l'élément pour lequel on effectue une requete
     method: 'GET'
   });
-  // console.log(requete); // afficher l'id dans la console - "status 200" requete OK //
+  // console.log(requete); // afficher l'id dans la console - "status 200" requete OK (status 400 >> erreur ) //
   
 
   if (!requete.ok) {
     alert('Un problème est survenu.');
   }
-  else { // selection du DOM où
+  else { // 
     let donnees = await requete.json();
-    // console.log(donnees); // verifier infos complètes du produit OK //
+    // console.log(donnees); //vérifie que les infos complètes du produit nous soient bien retournées OK //
     
+    let image = document.querySelector('.item__img');
+    let h1 = document.querySelector('#title');
+    let prix = document.querySelector('#price');
+    let description = document.querySelector('#description');
+    
+    // console.log(image); // sélection DOM OK
+    // console.log(h1); // OK
+    // console.log(prix); // OK
+    // console.log(description); // OK
+
+
+
   }
 }
 
