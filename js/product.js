2872fc4d1c7e705a1f418d9ab7170494bfe9c68f
Ruth Tshoms <tshomsruth@gmail.com>
2022-10-02 18:32:54 +0200

    suppression du var dataId plus utile

--- js/product.js
+++ js/product.js
@@ -5,7 +5,7 @@ let id = params.get("id"); // récupérer la valeur de l'id attribuée à la var
 // console.log(id); // 
 
 // Affichage du produit (objet) sélectionné par l'id en faisant une requête fetch //
-var donneesId; // portée globale de la variable qui sera réutilisée 
+//var dataId; // portée globale de la variable qui sera réutilisée 
 
 async function recupererId() {
   const requete = await fetch(`http://localhost:3000/api/products/${id}`, { // récupérer dynamiquement l'id de l'élément pour lequel on effectue une requete
@@ -17,17 +17,17 @@ async function recupererId() {
     alert('Un problème est survenu.');
   }
   else { // 
-    donneesId = await requete.json(); // portée globale de la variable //
-    // console.log(donneesId); //vérifie que les infos complètes du produit nous soient bien retournées //
+    let dataId = await requete.json(); // portée globale de la variable //
+    console.log(dataId); //vérifie que les infos complètes du produit nous soient bien retournées //
 
     // création dynamique de balises et insertions de l'élément (img) de la requete json dans le dom //
-    document.querySelector('.item__img').innerHTML = `<img src="${donneesId.imageUrl}" alt="Photographie d'un canapé"></img>`;
-    document.querySelector('#title').textContent = donneesId.name;
-    document.querySelector('#price').textContent = donneesId.price;
-    document.querySelector('#description').textContent = donneesId.description;
+    document.querySelector('.item__img').innerHTML = `<img src="${dataId.imageUrl}" alt="Photographie d'un canapé"></img>`;
+    document.querySelector('#title').textContent = dataId.name;
+    document.querySelector('#price').textContent = dataId.price;
+    document.querySelector('#description').textContent = dataId.description;
 
 
-    let couleurCanape = donneesId.colors; // on met l'objet colors de notre requete.JSON dans une variable 
+    let couleurCanape = dataId.colors; // on met l'objet colors de notre requete.JSON dans une variable 
     // console.log(couleurCanape); // affiche les couleurs des canapés
 
 
@@ -85,7 +85,7 @@ button.addEventListener('click', (e) => {
   let ajouterProduitLocalStorage = () => {
     produitLocalStorage.push(optionProduit);
     localStorage.setItem("produit", JSON.stringify(produitLocalStorage)); // (objet > string)
-    confirm("Votre commande de" + " " + choixQuantite + " " + donneesId.name + " " + choixCouleur + " vient d'être ajoutée au panier. Pour consulter votre panier, cliquer sur OK");
+    confirm("Votre commande de" + " " + choixQuantite + " " + dataId.name + " " + choixCouleur + " vient d'être ajoutée au panier. Pour consulter votre panier, cliquer sur OK");
     console.log(produitLocalStorage); // à enlever à la fin du projet 
   }
 
@@ -102,7 +102,7 @@ button.addEventListener('click', (e) => {
       // Empêcher la possibilité d'ajouter une quantité de produit supp à 100 en plusieurs clic //
       if (resultatFind.quantite <= 100) { 
       localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
-      confirm("Votre commande de" + " " + choixQuantite + " " + donneesId.name + " " + choixCouleur + " vient d'être ajoutée au panier. Pour consulter votre panier, cliquer sur OK");
+      confirm("Votre commande de" + " " + choixQuantite + " " + dataId.name + " " + choixCouleur + " vient d'être ajoutée au panier. Pour consulter votre panier, cliquer sur OK");
       }
       // Si le produit commandé n'est pas dans le panier//
     } else {
