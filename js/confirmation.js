e177efd38d16967d4bd279964a44f4183187e570
Ruth Tshoms <tshomsruth@gmail.com>
2022-10-03 12:53:46 +0200

    ajout commentaire

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,10 +1,8 @@
 /**
- * 
+ * Récupérer et insérer l'id généré par la requete POST dans le DOM
  */
-let params = new URL(document.location).searchParams; // - parcourir et afficher les param de la page
-// console.log(document.location); // 
-let id = params.get("id"); // récupérer la valeur de l'id attribuée à la variable id //
-console.log(id); // 
+let params = new URL(document.location).searchParams; 
+let id = params.get("id"); 
 
 document.querySelector('#orderId').textContent = id;
 
